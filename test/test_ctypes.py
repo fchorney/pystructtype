d4d17d62364a696b3ead0ac83067f568d58c3143
Fernando Chorney <github@djsbx.com>
2025-02-18 15:56:48 -0600

    Remove improperly formatted tests

--- test/test_ctypes.py
+++ test/test_ctypes.py
@@ -1,40 +1,15 @@
-from pystructtype import struct_dataclass, StructDataclass, uint8_t, TypeMeta, bits, BitsType, int16_t, char_t, \
-    double_t, float_t, uint16_t
-from .examples import SMXConfigType, TEST_CONFIG_DATA  # type: ignore
 from typing import Annotated
 
-def test_examples():
-    @struct_dataclass
-    class MyStruct(StructDataclass):
-        myNum: int16_t
-        myLetter: char_t
+from pystructtype import (
+    BitsType,
+    StructDataclass,
+    TypeMeta,
+    bits,
+    struct_dataclass,
+    uint8_t,
+)
 
-    s = MyStruct()
-    s.decode([4, 2, 65])
-    s.decode([4, 2, 65], little_endian=True)
-
-    @struct_dataclass
-    class MyStruct2(StructDataclass):
-        myFloat: float_t
-        myDouble: double_t
-
-    s2 = MyStruct2()
-    s2.decode([1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8])
-    s2.decode([1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8])
-
-    @struct_dataclass
-    class MyStruct3(StructDataclass):
-        myInts: Annotated[list[uint8_t], TypeMeta(size=4)]
-        myBiggerInts: Annotated[list[uint16_t], TypeMeta(size=2)]
-
-    s3 = MyStruct3()
-    s3.decode([0, 64, 128, 255, 16, 0, 255, 255])
-
-    @bits(uint8_t, {"lights_flag": 0, "platform_flag": 1})
-    class FlagsType(BitsType): ...
-
-    f = FlagsType()
-    f.decode([3])
+from .examples import TEST_CONFIG_DATA, SMXConfigType  # type: ignore
 
 
 def test_smx_config():
